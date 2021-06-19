29fb06f61143fa6064457e1fabbb66328f673a6f
Lavisha Bhambri <bhambrilavisha@gmail.com>
2021-06-19 17:09:33 +0530

    state added in CartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,7 +2,22 @@ import { div } from 'prelude-ls';
 import React from 'react';
 
 class CartItem extends React.Component{
+    // Defining state in CartItem
+    constructor () {
+        // Calling the constructor of parent class as we are inheriting from parent class
+        super();
+        this.state = {
+            price : 999,
+            title : 'Phone',
+            qty : 1,
+            img : ''
+        }
+    }
+
     render(){
+        // Getting the required properties from the above state object
+        const { price, title, qty } = this.state;
+
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -10,12 +25,17 @@ class CartItem extends React.Component{
                 </div>
 
                 <div className="right-block">
-                    <div style={{ fontSize: 25 }}>Phone</div>
-                    <div style={{ color: '#777' }}>Rs 999</div>
-                    <div style={{ color: '#777' }}>Qty: 1</div>
-                </div>
-                <div className="cart-item-actions">
-                    {/* Add Buttons here */}
+                    <div style={{ fontSize: 25 }}>{ title }</div>
+                    <div style={{ color: '#777' }}>Rs { price }</div>
+                    <div style={{ color: '#777' }}>Qty: { qty }</div>
+                    
+                    {/* For the icons at the right bottom */}
+                    <div className="cart-item-actions">
+                        {/* Add Buttons here */}
+                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992651.png"></img>
+                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png"></img>
+                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/512/1214/1214428.png"></img>
+                    </div>
                 </div>
             </div>
         );
