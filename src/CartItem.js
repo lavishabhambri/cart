f7a011904e66360aa5b28ab432a2a09dc17031cf
Lavisha Bhambri <bhambrilavisha@gmail.com>
2021-06-20 06:12:15 +0530

    Add total Amount in Cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,64 +1,3 @@
-// import { div } from 'prelude-ls';
-// import React from 'react';
-
-
-// const CartItem = (props) => {
-//     // Getting the required properties from the above state object
-//     const { price, title, qty } = props.product;
-//     const { product, onDecreasingQuantity, onIncreasingQuantity, onDeleteProduct } = props;
-
-//     return (
-//         <div className="cart-item">
-//             <div className="left-block">
-//                 <img style={styles.image} />
-//             </div>
-
-//             <div className="right-block">
-//                 <div style={{ fontSize: 25 }}>{title}</div>
-//                 <div style={{ color: '#777' }}>Rs {price}</div>
-//                 <div style={{ color: '#777' }}>Qty: {qty}</div>
-
-//                 {/* For the icons at the right bottom */}
-//                 <div className="cart-item-actions">
-//                     {/* Add Buttons here */}
-//                     <img
-//                         alt="increase"
-//                         className="action-icons"
-//                         src="https://image.flaticon.com/icons/png/512/992/992651.png"
-//                         onClick={() => onIncreasingQuantity(product)}
-//                     />
-//                     <img
-//                         alt="decrease"
-//                         className="action-icons"
-//                         src="https://image.flaticon.com/icons/png/512/992/992683.png"
-//                         onClick={() => onDecreasingQuantity(product)}
-//                     />
-//                     <img
-//                         alt="delete"
-//                         className="action-icons"
-//                         src="https://image.flaticon.com/icons/png/512/1214/1214428.png"
-//                         onClick={() => onDeleteProduct(product.id)}
-//                     />
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-
-// // Adding styles using an object
-// const styles = {
-//     image: {
-//         height: 110,
-//         width: 110,
-//         bordeerRadius: 4
-//     }
-// }
-
-
-// // Exporting this file
-// export default CartItem;
-
 import React from 'react';
 
 const CartItem = (props) => {
@@ -72,7 +11,7 @@ const CartItem = (props) => {
   return (
     <div className="cart-item">
       <div className="left-block">
-        <img style={styles.image} />
+        <img style={styles.image} src={product.img} />
       </div>
       <div className="right-block">
         <div style={ { fontSize: 25 } }>{title}</div>
